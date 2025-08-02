8f2a003ff5675e8fd75f2dd5603dbf1d6a303bda
Cristina <48189972+cristinaverse@users.noreply.github.com>
2025-08-02 03:53:28 +0200

    Create admin.py

---
+++ backend/djangoapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
